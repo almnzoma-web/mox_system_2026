aacbe30c978a33f24f8d9bc573dc70a5f0020d37
Mox <ao.almnzoma@gmai.com>
2026-07-30 08:28:30 +0200

    update: clean build and update pubspec assets

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -13,7 +13,7 @@ class StorageService {
 
   // رابط شيت قوقل السحابي الخاص بك
   static const String _scriptUrl =
-      "https://script.google.com/macros/s/AKfycbz3YkL-vJ5zpvq9MgU5nIDpxBjY5_fhbX_Yu52yrCJHrIdhbw0d-PCIpqTwU2g7oaat/exec";
+      "https://script.google.com/macros/s/AKfycbwmsJfKkALCzCmryey9dBJaI8j27p7JPn5l-mUiDF_ExLNjkPi5hkWRy_hFefiINfOu/exec";
 
   // تعريف المدير السيادي بكافة الحقول الشاملة (الواتساب، العمولات، الأصول، وغيرها)
   static final UserModel adminUser = UserModel(
